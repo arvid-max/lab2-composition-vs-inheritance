10e7aa6c282477ca86a43bc65529911b7566ab71
arvid-max <144478180+arvid-max@users.noreply.github.com>
2023-11-16 09:18:04 +0100

    Delete lab2-composition-vs-inheritance directory

--- lab2-composition-vs-inheritance/src/cars/Movable.java
+++
@@ -1,7 +0,0 @@
-package cars;
-
-public interface Movable {
-    void move();
-    void turnLeft();
-    void turnRight();
-}
--- lab2-composition-vs-inheritance/test/cars/Constants.java
+++
@@ -1,7 +0,0 @@
-package cars;
-
-public class Constants {
-
-    public static final double DOUBLE_EQUALS_ALLOWED_DIFF = 1e-8;
-
-}
